397a4f0eb41c252b0c9831aed56fa4b69c20078e
Rushikesh Patil <rushikeshp7558@gmail.com>
2024-02-11 14:22:30 +0530

    some changes in index

--- index.js
+++ index.js
@@ -9,16 +9,17 @@ const path = require('path');
 
 const app = express();
 require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth",userRoutes)
+app.use("/api/messages",messageRoute)
 
 app.get('/',(req,res)=>{
     app.use(express.static(path.resolve(__dirname,"public","dist")));
     res.sendFile(path.resolve(__dirname,"public","dist","index.html"));
 });
 
-app.use(cors());
-app.use(express.json());
-app.use("/api/auth",userRoutes)
-app.use("/api/messages",messageRoute)
+
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
